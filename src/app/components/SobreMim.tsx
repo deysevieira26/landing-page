a77b13de8eac1f169847728b9969e4b3f02a408f
deysevieira26 <deyservieira@gmail.com>
2025-04-15 15:13:39 -0300

    atualização arquivo sobre mim

--- src/app/components/SobreMim.tsx
+++ src/app/components/SobreMim.tsx
@@ -17,13 +17,13 @@ const SobreMim: React.FC = () => {
         <div className="w-2/3 pl-16">
           <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">Sobre Mim</h2>
           <p className="text-lg text-gray-700 mb-4 text-justify">
-            Sou estudante de Técnico em Informática para Internet e concluí o curso de Programador de Sistemas no SENAC - MA no ano passado. Embora ainda não tenha experiência profissional, estou em busca de um estágio ou uma oportunidade de trabalho que me permita colocar em prática o que aprendi e continuar me desenvolvendo na área de tecnologia.
+          Sou estudante do curso Técnico em Informática para Internet no SENAC-MA e, no ano passado, concluí o curso de Programador de Sistemas também no SENAC-MA. Embora ainda não tenha experiência profissional, busco uma oportunidade de estágio ou trabalho que me permita aplicar meus conhecimentos em desenvolvimento web e continuar me aprimorando nessa área.
           </p>
           <p className="text-lg text-gray-700 mb-4 text-justify">
-            Sou uma pessoa organizada, que valoriza o trabalho em equipe e sempre busca aprender mais. Tenho grande interesse em estar por dentro das novidades do setor, o que me motiva a acompanhar as últimas tendências e inovações no campo da informática e programação.
+          Sou uma pessoa organizada, com facilidade para trabalhar em equipe e sempre motivada a aprender novas tecnologias. Tenho grande interesse em me manter atualizada sobre as últimas tendências e inovações no desenvolvimento de websites e aplicações, além de ser entusiasta das melhores práticas de programação e design.
           </p>
           <p className="text-lg text-gray-700 text-justify">
-            Estou disposta a enfrentar novos desafios e contribuir com minha dedicação e entusiasmo para o crescimento de qualquer equipe ou projeto que eu faça parte.
+          Estou disposta a enfrentar novos desafios e contribuir com minha dedicação e entusiasmo para o crescimento de qualquer equipe ou projeto na área de desenvolvimento web.
           </p>
         </div>
       </div>
